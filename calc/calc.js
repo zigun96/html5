02fcf6558edae700da40bf33d15f3e75c420d858
cha_jae_geon <jaegun920@naver.com>
2022-11-21 17:30:18 +0900

    JavaScript 계산기 만들기 eval() 함수 이용 commit

--- calc/calc.js
+++ calc/calc.js
@@ -1,48 +1,20 @@
 var buttons = document.querySelectorAll("button");
 var input = document.querySelector("input");
-var operand = undefined; // 값을 대입하지 않았다.
-var flag = false; // 연산 버튼 토글
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function (event) {
     // click event의 target
-    var text = event.target.innerText;
+    var text = event.target.value;
     console.log(text);
     switch (text) {
-      case "/":
-        operand = parseInt(input.value);
-        flag = true;
+      case "=": // eval 연산
+        input.value = eval(input.value);
         break;
-      case "x":
-        operand = parseInt(input.value);
-        flag = true;
+      case "AC": // 텍스트 초기화
+        input.value = '';
         break;
-      case "-":
-        operand = parseInt(input.value);
-        flag = true;
-        break;
-      case "+":
-        operand = parseInt(input.value);
-        flag = true;
-        break;
-      default:
-        // 연산 버튼을 눌렀을 때 수를 새로 쓴다.
-        if (flag == true){
-          input.value = text;
-          flag = false;
-        }else if(flag == false){
-          input.value += text;
-        }       
+      default: // 텍스트 연결
+        input.value += text;
     }
-    // 1. 변수 사용
-    // var origin = document.querySelectorI("input").value;
-    // document.querySelector("input").value = origin + text;
-
-    // 2. 식 하나로
-    // document.querySelector('input').value =
-    // document.querySelector('input').value + text;
-
-    // 3. 축약 연산자
-    // document.querySelector("input").value = document.querySelector("input").value + text;
   });
 }
